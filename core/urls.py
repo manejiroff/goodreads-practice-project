b4ecc4c21860012f239ee5f7bbc1217ae6f92c2d
manejiroff <manejiroff@gmail.com>
2026-04-19 10:59:16 +0500

    first commit

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # apps urls
+    path('', include('books.urls')),
+    path('users/', include('users.urls')),
+]
